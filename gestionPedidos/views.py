6c824b6624ef5a72fda74425f69391711d7418b1
Daniel <danielaac2020@gmail.com>
2020-09-17 23:00:54 -0500

    Video 21 apiforms

--- gestionPedidos/views.py
+++ gestionPedidos/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from gestionPedidos.models import Articulos
 from django.core.mail import send_mail
 from django.conf import settings
+from gestionPedidos.forms import FormularioContacto
 
 # Create your views here.
 
@@ -24,14 +25,26 @@ def buscar(request):
 
 def contacto(request):
     if request.method=='POST':
-        to = request.POST['email']
-        subject = request.POST['Asunto']
-        message = request.POST['mensaje'] 
-        email_from = settings.EMAIL_HOST_USER 
-        print([to,])
-       # recipient_list =["daniel.aguirrec@udea.edu.co"]
-        send_mail(subject, message, email_from, [to])
-        return render(request,"gracias.html")
+        miFormulario = FormularioContacto(request.POST)
+        if miFormulario.is_valid(): 
+            infForm = miFormulario.cleaned_data
+            send_mail(infForm['asunto'],
+            infForm['mensaje'],
+            infForm.get('email',''),['daniel.aguirrec@udea.edu.co'],)
+            return render(request, "gracias.html")
     else:
-         return render(request,"contacto.html")
+        miFormulario = FormularioContacto()
+    return render(request,"formulario_contacto.html",{"form":miFormulario})
+
+        
+"""to = request.POST['email']
+subject = request.POST['Asunto']
+message = request.POST['mensaje'] 
+email_from = settings.EMAIL_HOST_USER 
+print([to,])
+# recipient_list =["daniel.aguirrec@udea.edu.co"]
+send_mail(subject, message, email_from, [to])
+return render(request,"gracias.html")
+else:
+    return render(request,"contacto.html")"""
 
